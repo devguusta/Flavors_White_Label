0a43cc7bab02216e4e8e96dc38bcb6c04d3edfa9
Gustavo Silva Candido Cabral <gustavo.cabral@rarolabs.com.br>
2022-04-05 20:38:21 -0300

    cria diversos flavures como exemplo

---
+++ lib/flavors.dart
@@ -0,0 +1,28 @@
+enum Flavor {
+  MAGALU_DEV,
+  MAGALU_PROD,
+  AMERICANAS_DEV,
+  AMERICANAS_PROD,
+}
+
+class F {
+  static Flavor? appFlavor;
+
+  static String get name => appFlavor?.name ?? '';
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.MAGALU_DEV:
+        return 'Magalu App Dev';
+      case Flavor.MAGALU_PROD:
+        return 'Magalu App';
+      case Flavor.AMERICANAS_DEV:
+        return 'Americanas App Dev';
+      case Flavor.AMERICANAS_PROD:
+        return 'Americanas App';
+      default:
+        return 'title';
+    }
+  }
+
+}
