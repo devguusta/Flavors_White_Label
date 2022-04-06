20c368cdb2da1e2fa4106c2d615893e1ac2c277e
Gustavo Silva Candido Cabral <gustavo.cabral@rarolabs.com.br>
2022-04-06 14:21:32 -0300

    exibi url base

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:white_label/styles/texts_example.dart';
 
+import '../core/app_constants.dart';
 import '../flavors.dart';
 
 class MyHomePage extends StatelessWidget {
@@ -12,11 +13,21 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(F.title),
       ),
-      body: Center(
-        child: Text(
-          'Hello ${F.title}',
-          style: F.appFlavor!.getStyle(),
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Hello ${F.title}',
+              style: F.appFlavor!.getStyle(),
+            ),
+          ),
+          Center(
+            child: Text(
+              'URL BASE: ${ApiRoutes().getUrlBase(F.appFlavor!)}',
+              style: F.appFlavor!.getStyle(),
+            ),
+          ),
+        ],
       ),
     );
   }
